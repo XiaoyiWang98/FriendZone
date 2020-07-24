a2d4002143d078b28cd71cbb2ff41812d491d3d3
XiaoyiWang98 <seanwang20121997@hotmail.com>
2020-07-23 22:46:49 -0400

    build file deleted

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../styles/App.css';
+import TopBar from "./TopBar";
+import Main from "./Main";
+import { TOKEN_KEY } from '../constants';
+class App extends React.Component{
+    state = {
+        isLoggedIn: Boolean(localStorage.getItem(TOKEN_KEY)),
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    handleLoginSucceed = (token) => {
+        localStorage.setItem(TOKEN_KEY, token)
+        this.setState({ isLoggedIn: true });
+    }
+
+    handleLogout = () => {
+        localStorage.removeItem(TOKEN_KEY);
+        this.setState({ isLoggedIn: false });
+    }
+
+    render(){
+        return (
+            <div className="App">
+                <TopBar handleLogout={this.handleLogout}
+                        isLoggedIn={this.state.isLoggedIn}
+                />
+                <Main
+                    handleLoginSucceed={this.handleLoginSucceed}
+                    isLoggedIn={this.state.isLoggedIn}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
+
---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {Register} from "./Register";
+import Login from './Login'
+import Home from "./Home"
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="main">
+                <Switch>
+                    <Route path="/register" component={Register}/>
+                    <Route path="/login" render={this.getLogin}/>
+                    <Route path="/home" render={this.getHome}/>
+                </Switch>
+            </div>
+        );
+    }
+
+    getLogin = () => {
+        return this.props.isLoggedIn
+            ? <Redirect to = "/home"/>
+            : <Login handleLoginSucceed={this.props.handleLoginSucceed}/>;
+    }
+
+    getHome = () =>{
+        return this.props.isLoggedIn
+            ? <Home/>
+            : <Redirect to = "/login"/>
+    }
+
+}
+
+export default Main;
